5f0f9b9df85711173eabaea9f3ad58045df9bb17
Joyce Robbins <jtr13@users.noreply.github.com>
2026-04-08 15:29:42 -0400

    Add files via upload

---
+++ apr6.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(sf)
+library(readxl)
+
+nypp <- read_sf("nypp_25c/nypp.shp")
+
+head(nypp)
+
+ggplot(nypp) +
+  geom_sf() +
+  geom_sf_text(aes(label = Precinct), size = 2) +
+  theme_void()
+
+ny_crime <- read_xls("felony.xls", skip = 2) |> 
+  fill(PCT) |> 
+  filter(CRIME == "ROBBERY") |> 
+  select(Precinct = PCT, CRIME, Robbery = `2021`) |> 
+  mutate(Precinct = as.numeric(Precinct))
+
+plot_data <- left_join(nypp, ny_crime)
+
+ggplot(plot_data) +
+  geom_sf(aes(fill = Robbery)) + 
+  geom_sf_text(aes(label = Precinct), size = 2,
+               color = "white") +
+  theme_void()
